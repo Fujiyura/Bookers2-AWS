7de17330e10c51700fe219b3fc83526d4e70947f
Fujiyura <it@fujiyura.net>
2020-10-16 22:27:24 +0900

    add model validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+	validates :name, presence: true, length: { in: 2..20 }
+	validates :introduction, length: {maximum: 50}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
